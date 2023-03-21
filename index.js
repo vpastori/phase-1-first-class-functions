83fa6279e3c8cd7be2cafac82535aa6f083642d2
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-03-21 15:20:38 -0400

    finished lab

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+    };
+}
